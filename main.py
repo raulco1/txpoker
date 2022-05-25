97438b8a3daad9e69602c89287fefaf8b5e51b3a
raulco1 <racorrea@ucsd.edu>
2022-05-24 18:31:03 -0700

    Initial Project

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #import kivy module
-import random
 
 import kivy
 
@@ -23,7 +22,14 @@ from kivy.uix.button import Button
 # another way used to run kivy app
 from kivy.base import runTouchApp
 
+
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.config import Config
+
 Config.set('graphics', 'resizable', True)
 
 
@@ -35,6 +41,23 @@ import random
 import poker
 
 
+def txt_to_image(card):
+    suits = {'♣': 'spades', '♦': 'diamonds', '♥': 'hearts', '♠': 'clubs' }
+    special_ranks = {'T': '10', 'J': 'jack', 'Q': 'queen', 'K': 'king', 'A': 'ace'}
+    print(card)
+
+    rank = str(card)[0]
+    suit = str(card)[1]
+
+    if rank in special_ranks:
+        image = f'{special_ranks[rank]}_of_{suits[suit]}'
+    else:
+        image = f'{rank}_of_{suits[suit]}'
+    return image
+
+
+
+
 
 deck = list(Card)
 random.shuffle(deck)
@@ -42,26 +65,60 @@ random.shuffle(deck)
 #Player 1
 p1Hand = [deck.pop() for __ in range(2)]
 
-def txt_to_image(card):
-    print("hrer" , card[1])
-    suits = {'♣': 'spades', '♦': 'diamonds', '♥': 'hearts', '♠': 'clubs' }
-    special_ranks = {'T': '10', 'J': 'jack', 'Q': 'queen', 'K': 'king', 'A': 'ace'}
-
-    #image = f'{special_ranks[card[0]]}_of_{suits[card[1]]}'
-    return suits[str(card[1])]
-
-
-
-
-
-print(p1Hand)
-print(p1Hand[0])
-print(txt_to_image(p1Hand[0]))
-
 
 flop = [deck.pop() for __ in range(3)]
 turn = deck.pop()
 river = deck.pop()
+
+
+print(txt_to_image(p1Hand[0]))
+
+
+
+
+
+class TxPoker(App):
+
+    def build(self):
+
+        Rl = FloatLayout()
+
+        p1card1 = Button(size_hint=(.15, .3),
+                     background_normal = 'PNG-cards-1.3/back_of_card.jpg',
+                     pos_hint = {'center_x':.3, 'center_y':.2},
+                     background_down = 'PNG-cards-1.3/' + txt_to_image(p1Hand[0]) + '.png',
+                     text='Hello world')
+
+        print(p1card1)
+
+
+        p1card2 = Button(size_hint=(.15, .3),
+                         background_normal='PNG-cards-1.3/back_of_card.jpg',
+                         pos_hint={'center_x': .7, 'center_y': .2},
+                         background_down='PNG-cards-1.3/' + txt_to_image(p1Hand[1]) + '.png',
+                         text='Hello world')
+
+
+
+        # adding widget i.e button
+        Rl.add_widget(p1card1)
+        Rl.add_widget(p1card2)
+        #Rl.add_widget(btn1)
+
+        # return the layout
+        return Rl
+
+
+
+
+
+
+# run the app
+if __name__ == "__main__":
+    TxPoker().run()
+
+
+
 
 #
 # print(flop.Suit)
